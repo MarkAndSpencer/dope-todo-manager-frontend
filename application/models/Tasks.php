bf12c39b3bb96c58648b98ea72fb25d981f2206b
maennis <mark.andrew.ennis@gmail.com>
2017-12-03 14:33:43 -0800

    Initial front end commit

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,16 +1,34 @@
 <?php
 
 /**
+ * Modified to use REST client to get port data from our server.
+ */
+define('REST_SERVER', 'http://backend.local');  // the REST server host
+define('REST_PORT', $_SERVER['SERVER_PORT']);   // the port you are running the server
+
+/**
 * Model for Tasks on the todo list
 */
-class Tasks extends XML_Model
+class Tasks extends Memory_Model
 {
     /**
     * ctor
     */
     public function __construct()
     {
-        parent::__construct(APPPATH . '../data/tasks.xml', 'id', 'Task');
+        parent::__construct();
+        $this->_fields = array('id','task','priority','size','group','deadline','status','flag');
+    }
+
+    public function load() {
+        // load our data from the REST backend
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $this->_data =  $this->rest->get('job');
+
+        $one = array_values((array) $this->_data);
+        $this->_fields = array_keys((array)$one[0]);
+        $this->reindex();
     }
 
     function getCategorizedTasks()
@@ -49,6 +67,7 @@ class Tasks extends XML_Model
 
     public function all()
     {
+        $this->load();
         $ret = array();
         foreach (parent::all() as $t) {
             $ret[$t->id] = $this->task->create($t);
@@ -56,9 +75,41 @@ class Tasks extends XML_Model
         return $ret;
     }
 
-    public function get($id, $key2 = null)
+    public function get($key, $key2 = null)
     {
-        return $this->task->create(parent::get($id, $key2));
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        return $this->rest->get('job/' . $key);
+    }
+
+    function delete($key, $key2 = null)
+    {
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $this->rest->delete('job/' . $key);
+        $this->load();
+    }
+
+    function update($record)
+    {
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $key = $record->{$this->_keyfield};
+        $retrieved = $this->rest->put('job/' . $key, $record);
+        $this->load();
+    }
+
+    function add($record)
+    {
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $key = $record->{$this->_keyfield};
+        $retrieved = $this->rest->post('job/' . $key, $record);
+        $this->load();
+    }
+
+    protected function store()
+    {
     }
 
 }
